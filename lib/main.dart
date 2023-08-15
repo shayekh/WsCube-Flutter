e940a793e221bc05a3e893b7e4757ebd96489f7a
shayekh <shayekhebnemizan@gmail.com>
2023-08-16 01:05:15 +0600

    Callback Function

--- lib/main.dart
+++ lib/main.dart
@@ -56,143 +56,33 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+
+  callBack() {
+    print("Clicked");
   }
+
 
   @override
   Widget build(BuildContext context) {
-    var arrColors = [
-      Colors.amber,
-      Colors.red,
-      Colors.blue,
-      Colors.lightGreenAccent,
-      Colors.grey
-    ];
-
     return Scaffold(
         appBar: AppBar(
           // TRY THIS: Try changing the color here to a specific color (to
           // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
           // change color while the other colors stay the same.
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          backgroundColor: Theme
+              .of(context)
+              .colorScheme
+              .inversePrimary,
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
         ),
-        body:
-            // GridView.count(crossAxisCount: 2,
-            //     crossAxisSpacing: 3,
-            //     mainAxisSpacing: 5,
-            //     children: [
-            //   Padding(
-            //     padding: const EdgeInsets.all(8.0),
-            //     child: Container(color: arrColors[0],),
-            //   ),
-            //   Padding(
-            //     padding: const EdgeInsets.all(8.0),
-            //     child: Container(color: arrColors[1],),
-            //   ),
-            //   Padding(
-            //     padding: const EdgeInsets.all(8.0),
-            //     child: Container(color: arrColors[2],),
-            //   ),
-            //   Padding(
-            //     padding: const EdgeInsets.all(8.0),
-            //     child: Container(color: arrColors[3],),
-            //   ),
-            //   Padding(
-            //     padding: const EdgeInsets.all(8.0),
-            //     child: Container(color: arrColors[4],),
-            //   ),
-            //   Padding(
-            //     padding: const EdgeInsets.all(8.0),
-            //     child: Container(color: arrColors[1],),
-            //   ),
-            //   Padding(
-            //     padding: const EdgeInsets.all(8.0),
-            //     child: Container(color: arrColors[2],),
-            //   ),
-            //   Padding(
-            //     padding: const EdgeInsets.all(8.0),
-            //     child: Container(color: arrColors[3],),
-            //   ),
-            //
-            //
-            //
-            //
-            // ]),
+        body: ElevatedButton(
+          onPressed: callBack,
+          child: const Text('Click Me!'),
 
-            // GridView.extent(maxCrossAxisExtent: 100,
-            //
-            //     children: [
-            //       Padding(
-            //         padding: const EdgeInsets.all(8.0),
-            //         child: Container(color: arrColors[0],),
-            //       ),
-            //       Padding(
-            //         padding: const EdgeInsets.all(8.0),
-            //         child: Container(color: arrColors[1],),
-            //       ),
-            //       Padding(
-            //         padding: const EdgeInsets.all(8.0),
-            //         child: Container(color: arrColors[2],),
-            //       ),
-            //       Padding(
-            //         padding: const EdgeInsets.all(8.0),
-            //         child: Container(color: arrColors[3],),
-            //       ),
-            //       Padding(
-            //         padding: const EdgeInsets.all(8.0),
-            //         child: Container(color: arrColors[4],),
-            //       ),
-            //       Padding(
-            //         padding: const EdgeInsets.all(8.0),
-            //         child: Container(color: arrColors[1],),
-            //       ),
-            //       Padding(
-            //         padding: const EdgeInsets.all(8.0),
-            //         child: Container(color: arrColors[2],),
-            //       ),
-            //       Padding(
-            //         padding: const EdgeInsets.all(8.0),
-            //         child: Container(color: arrColors[3],),
-            //       ),
-            //
-            //
-            //
-            //
-            //     ]),
-
-            // GridView.builder(itemBuilder: (context, index) {
-            //   return Container(color: arrColors[index], );
-            // }, itemCount: arrColors.length, gridDelegate:  const SliverGridDelegateWithFixedCrossAxisCount(
-            //   crossAxisCount: 3,
-            //    mainAxisExtent: 100,
-            //   crossAxisSpacing: 5.0,
-            //   mainAxisSpacing: 5.0,
-            // ))
-
-            GridView.builder(
-                itemBuilder: (context, index) {
-                  return Container(
-                    color: arrColors[index],
-                  );
-                },
-                itemCount: arrColors.length,
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 100,
-                  crossAxisSpacing: 5.0,
-                  mainAxisSpacing: 5.0,
-                )));
+        )
+    );
   }
 }

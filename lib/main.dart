1a1e0186cf9a4cd7abe76588930a4e5eb7bf9e26
Shayekh <shayekhebnemizan@gmail.com>
2023-09-07 09:45:51 +0600

    Stack-Widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,48 +12,66 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Split Widget'),
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Container(
-                color: Colors.green,
-              ),
-            ),Expanded(
-              flex: 1,
-              child: Container(
+        appBar: AppBar(
+          // TRY THIS: Try changing the color here to a specific color (to
+          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+          // change color while the other colors stay the same.
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Container(
+          width: 300,
+          height: 300,
+          child: Stack(
+            children: [
+              Container(
+                width: 200,
+                height: 200,
                 color: Colors.blue,
               ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                color: Colors.grey,
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+              Positioned(
+                left: 21,
+                top: 21,
+                // right: 21,
+                // bottom: 21,
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.grey,
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
